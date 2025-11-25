e2b43e8512e13e37119925b96be2de3ec5f5f2a6
Sue-star-1 <wangarimm@outlook.com>
2025-11-25 21:31:34 +0300

    implemented initial eventpage

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LightRays from "./components/LightRays";
 import HomePage from "./pages/HomePage";
 import NavBar from "./components/NavBar";
+import Eventpage from "./pages/Eventpage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <NavBar />
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/events" element={<Eventpage />} />
         </Routes>
       </BrowserRouter>
 
